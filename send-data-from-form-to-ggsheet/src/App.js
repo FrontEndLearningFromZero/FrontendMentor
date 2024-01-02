615049188f60e1c904f6034d5b48c0b2d284cc67
khoadoktmt <30308343+khoadoktmt@users.noreply.github.com>
2024-01-02 17:38:59 +0700

    Add files via upload

--- send-data-from-form-to-ggsheet/src/App.js
+++ send-data-from-form-to-ggsheet/src/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const postAction = "https://script.google.com/macros/s/AKfycbyidsrotkrWIZvR9MZgxWMReyMXnVH3AxwA2HSWiRLqx67v1UeY09sz3Qfi6kdevRj-WQ/exec"
+  const postAction = "https://script.google.com/macros/s/AKfycbydYApBb4vn1i_IFI8NBQC1cK24X4A4rJWyI1lIfNf-SltWVAt-_us1-9PQq5M5Q-PeEQ/exec"
+  const getAction = "https://script.google.com/macros/s/AKfycbxZDH9JcrBdp2MTIrV5uK8_K5twH77zc-bS8ITr6ug08tEbVj3LOLcZahxAD5BttpdFlg/exec"
+  
   const userInfo = {
     name: 'John',
     mail: 'john@gmail.com',
@@ -12,6 +14,27 @@ export default function App() {
   }
   
   const [ user, setUser ] = useState(userInfo)
+  let header = []
+  let content = []
+
+  useEffect(()=>{
+    fetch(getAction)
+    .then(res => res.json())
+    .then(data => {
+      header = data[0]
+
+      let tmpArr = data.slice(1)
+      for(var i = 0; i < tmpArr.length; i++) {
+        let obj = {}
+        var rowArr = tmpArr[i]
+        for(var j=0; j < rowArr.length; j++) {
+          obj[header[j]] = rowArr[j]
+        }
+        content.push(obj)
+      }
+    })
+    console.log(content)
+  },[])
 
   return (
     <div className="App">
@@ -25,6 +48,7 @@ export default function App() {
           <button type="submit" name="Submit" value="submit">Submit</button>
         </form>
       </div>
+
     </div>
   );
 }
